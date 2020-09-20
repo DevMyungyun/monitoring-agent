35d347285acb96a116487ba37416ba45e7ea3a50
myungyun <myungyun.cho82@gmail.com>
2020-09-20 15:41:28 +0900

    update generateMatrix command.go

--- command/command.go
+++ command/command.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"strings"
 )
 
-func CheckDisk() {
+func CheckDisk() []string {
 	path, _ := exec.LookPath("go")
 	fmt.Println(path)
 
@@ -15,4 +16,42 @@ func CheckDisk() {
 		log.Fatal(err)
 	}
 	fmt.Printf("The output is %s\n", out)
+
+	stringOut := string(out)
+	arrOut := strings.Split(stringOut,"\r\n")
+
+	matrix := generateMatrix(arrOut)
+
+	for _, row := range matrix {
+		for i, element := range row {
+			if(i == 3) {
+				fmt.Print(element, " ")
+			}
+		}
+		fmt.Println()
+	}
+	
+	
+
+	fmt.Print("end>>>", matrix)
+}
+
+
+func generateMatrix(out []string) [][]string {
+
+	matrix := make([][]string, len(out))
+	for i := 0; i < len(out); i++ {
+		// fmt.Println("tmp>>>", tmpArr)
+		matrix[i] = make([]string, 0, len(out))
+		// vector := make([]string, len(out))
+		tmpArr := strings.Split(out[i]," ")
+		for j := 0; j<len(tmpArr); j++ {
+			if tmpArr[j] != "" {
+				matrix[i] = append(matrix[i], tmpArr[j])
+				// fmt.Println("tmp>>>", tmpArr[j])
+			} 
+		}
+	}
+
+	return matrix
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/robfig/cron/v3 v3.0.1
 	github.com/sirupsen/logrus v1.6.0
+	github.com/thoas/go-funk v0.7.0
 	monitoring-agent/command v0.0.0
 )
 
